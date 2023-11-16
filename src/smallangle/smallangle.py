bf9b71268d2fe59b508ea03951ffb61019b8fa18
IsabellaSchoehuijs <isabellaschoehuijs@gmail.com>
2023-11-16 11:49:34 +0100

    groepen gemaakt, commands en docstrings gemaakt.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,14 +4,44 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=5,
+    help="Number of values between 0 and 2pi",
+    show_default=True,  # show default in help
+)
 def sin(number):
+    """Calculates the sine for a given number of values between 0 and 2pi.
+
+    Args:
+        number (integer): number of values between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=5,
+    help="Number of values between 0 and 2pi",
+    show_default=True,  # show default in help
+)
 def tan(number):
+    """Calculates the tangent for a given number of values between 0 and 2pi.
+
+    Args:
+        number (integer): number of values between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
@@ -19,4 +49,5 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+    
